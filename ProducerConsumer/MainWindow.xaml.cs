4932b95664c54378f3a97e1391f805fb6adb4a38
Vladimir <vvbedash@gmail.com>
2023-11-16 19:13:07 +0600

    Producer-Consumers - Semaphore using ring buffer has been added; - Interface inputs for producer and consumers has been added - Consumers methods have been added;

--- ProducerConsumer/MainWindow.xaml.cs
+++ ProducerConsumer/MainWindow.xaml.cs
@@ -1,28 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ProducerConsumer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly RingBuffer<char> _circularBuffer;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
         public MainWindow()
         {
             InitializeComponent();
+            _circularBuffer = new RingBuffer<char>(10);
+            _cancellationTokenSource = new CancellationTokenSource();
+            StartConsumers();
+        }
+
+        private void TextInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                _circularBuffer.Enqueue(textBox.Text.ToCharArray().FirstOrDefault());
+                textBox.TextChanged -= TextInput_TextChanged;
+                textBox.Text = string.Empty;
+                textBox.TextChanged += TextInput_TextChanged;
+            }
+        }
+
+        private void StartConsumers()
+        {
+            Task.Run(() => Consume(_cancellationTokenSource.Token, field1, char.IsLetter));
+            Task.Run(() => Consume(_cancellationTokenSource.Token, field2, char.IsDigit));
+            Task.Run(() => Consume(_cancellationTokenSource.Token, field3, c => !char.IsLetterOrDigit(c)));
+        }
+
+        private void Consume(CancellationToken cancellationToken, TextBox textBox, Predicate<char> predicate)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                if (_circularBuffer.CheckDeq(predicate))
+                {
+                    var data = _circularBuffer.Dequeue();
+                    if (!string.IsNullOrEmpty(data.ToString()))
+                    {
+                        Dispatcher.Invoke(() => textBox.Text += data);
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1);
+                }
+            }
         }
     }
 }
---
+++ ProducerConsumer/RingBuffer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace ProducerConsumer;
+
+public class RingBuffer<T>
+{
+    private readonly T[] _buffer;
+    private readonly int _capacity;
+    private int _head;
+    private int _tail;
+
+    private readonly SemaphoreSlim _empty;
+    private readonly SemaphoreSlim _mutex;
+    private readonly SemaphoreSlim _full;
+
+    public RingBuffer(int capacity)
+    {
+        if (capacity <= 0)
+            throw new ArgumentException("Capacity must be greater than zero.");
+
+
+        _capacity = capacity;
+        _buffer = new T[capacity];
+        _head = 0;
+        _tail = 0;
+
+        _full = new SemaphoreSlim(0, capacity);
+        _empty = new SemaphoreSlim(capacity, capacity);
+        _mutex = new SemaphoreSlim(1, 1);
+    }
+
+    public bool CheckDeq(Predicate<T> predicate)
+    {
+        _full.Wait();
+        _mutex.Wait();
+        var result = predicate(_buffer[_head]);
+        _mutex.Release();
+        _full.Release();
+        return result;
+    }
+
+
+    public void Enqueue(T item)
+    {
+        _empty.Wait();
+        _mutex.Wait();
+        _buffer[_tail] = item;
+        _tail = (_tail + 1) % _capacity;
+        _mutex.Release();
+        _full.Release();
+    }
+
+
+    public T Dequeue()
+    {
+        _full.Wait();
+        _mutex.Wait();
+        var item = _buffer[_head];
+        _head = (_head + 1) % _capacity;
+        _mutex.Release();
+        _empty.Release();
+        return item;
+    }
+}
